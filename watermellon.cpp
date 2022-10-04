d62a16572548856f686e3d4caf3fc9b4094b859d
Sachin Vardhan <vardhansachin08@gmail.com>
2022-10-04 11:25:56 +0530

    CodeforceS 4A

---
+++ watermellon.cpp
@@ -0,0 +1,18 @@
+// Solution By Sachin Vardhan
+// Only for Educational Purpose
+// Codeforces 4A Watermelon 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a;
+	cin>>a;
+	if(a%2==0 && a>2){
+		cout<<"YES"<<endl;
+	}
+	else{
+		cout<<"NO"<<endl;
+	}
+	return 0;
+}
